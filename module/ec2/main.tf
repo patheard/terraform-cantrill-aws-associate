1131708d998a3f9f773d8f6ecae716e0130caac1
Pat Heard <patrick.heard@canada.ca>
2021-04-04 09:30:20 -0400

    fix(ec2): ignore tfsec open egress cidr block warning

--- module/ec2/main.tf
+++ module/ec2/main.tf
@@ -29,7 +29,7 @@ resource "aws_security_group" "allow_ssh" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] #tfsec:ignore:AWS009  Allow all egress for demo instance
   }
 
   tags = {
