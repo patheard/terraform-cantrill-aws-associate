50a3e5d343687e24716522a5db023f63264dae55
Pat Heard <patrick.heard@canada.ca>
2021-04-06 14:37:50 -0400

    feat: IAM group with policy attached

---
+++ lab/iam-group-policy/main.tf
@@ -0,0 +1,51 @@
+locals {
+  full_group_name = "${var.group_name}-${var.env}"
+}
+
+data "aws_iam_user" "ren" {
+  user_name = var.user_name
+}
+
+data "aws_s3_bucket" "catpics" {
+  bucket = var.catpics_bucket_name
+}
+
+resource "aws_iam_group" "this" {
+  name = local.full_group_name
+}
+
+resource "aws_iam_group_membership" "this" {
+  name = "${local.full_group_name}-group-membership"
+
+  users = [
+    data.aws_iam_user.ren.user_name
+  ]
+
+  group = aws_iam_group.this.name
+}
+
+resource "aws_iam_group_policy" "this" {
+  name  = "AllS3ExceptCats"
+  group = aws_iam_group.this.name
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "AllObjects",
+        "Effect" : "Allow",
+        "Action" : "s3:*",
+        "Resource" : "*"
+      },
+      {
+        "Sid" : "NoCatsForYou",
+        "Effect" : "Deny",
+        "Action" : "s3:*",
+        "Resource" : [
+          "${data.aws_s3_bucket.catpics.arn}",
+          "${data.aws_s3_bucket.catpics.arn}/*"
+        ]
+      }
+    ]
+  })
+}
---
+++ lab/iam-group-policy/variables.tf
@@ -0,0 +1,15 @@
+variable "env" {
+  type = string
+}
+
+variable "catpics_bucket_name" {
+  type = string
+}
+
+variable "group_name" {
+  type = string
+}
+
+variable "user_name" {
+  type = string
+}
