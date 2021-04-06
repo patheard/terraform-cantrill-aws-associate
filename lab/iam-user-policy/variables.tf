b67287a10a71784fe38b62ce22fef74daaff04ac
Pat Heard <patrick.heard@canada.ca>
2021-04-06 13:49:02 -0400

    feat: lab with IAM user and policy attached (#11)

---
+++ lab/iam-user-policy/variables.tf
@@ -0,0 +1,9 @@
+variable "env" {
+  description = "Environment you are deploying to (appended to bucket name)"
+  type        = string
+}
+
+variable "name" {
+  description = "The IAM account's name"
+  type        = string
+}
