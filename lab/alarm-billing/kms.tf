3bf1bfadcc4ba641254baade069c88a8fefb617d
Pat Heard <patrick.heard@canada.ca>
2021-04-09 07:57:27 -0400

    feat: fix alarm CMK policy
    Only need to define key adminstrators at this point.

--- lab/alarm-billing/kms.tf
+++ lab/alarm-billing/kms.tf
@@ -4,37 +4,44 @@ resource "aws_kms_key" "alarm_encryption_key" {
   description         = "Alarm ${var.env} encryption key"
   enable_key_rotation = true
 
-  # This policy allows encryption/decryption in Cloudwatch
-  policy = <<EOF
-{
-  "Version" : "2012-10-17",
-  "Id" : "key-default-1",
-  "Statement" : [ 
-    {
-      "Sid" : "Allow IAM user to manage CMK",
-      "Effect" : "Allow",
-      "Principal" : {
-        "AWS" : "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/iamadmin"
+  # Create key administrators
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Id" : "key-default-1",
+    "Statement" : [
+      {
+        "Sid" : "Allow IAM root user to manage CMK",
+        "Effect" : "Allow",
+        "Principal" : {
+          "AWS" : "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        },
+        "Action" : "kms:*",
+        "Resource" : "*"
       },
-      "Action" : "kms:*",
-      "Resource" : "*"
-    },
-    {
-      "Sid": "Allow CloudWatch and SES to use CMK",
-      "Effect": "Allow",
-      "Principal": {
-          "Service": [
-            "cloudwatch.amazonaws.com",
-            "ses.amazonaws.com"
-          ]
-      },
-      "Action": [
-          "kms:Decrypt",
-          "kms:GenerateDataKey*"
-      ],
-      "Resource": "*"
-    }
-  ]
-}
-EOF  
+      {
+        "Sid" : "Allow access for Key Administrators",
+        "Effect" : "Allow",
+        "Principal" : { "AWS" : [
+          "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/iamadmin"
+        ] },
+        "Action" : [
+          "kms:Create*",
+          "kms:Describe*",
+          "kms:Enable*",
+          "kms:List*",
+          "kms:Put*",
+          "kms:Update*",
+          "kms:Revoke*",
+          "kms:Disable*",
+          "kms:Get*",
+          "kms:Delete*",
+          "kms:TagResource",
+          "kms:UntagResource",
+          "kms:ScheduleKeyDeletion",
+          "kms:CancelKeyDeletion"
+        ],
+        "Resource" : "*"
+      }
+    ]
+  })
 }
